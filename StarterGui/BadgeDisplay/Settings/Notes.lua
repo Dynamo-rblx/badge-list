f3cd5272b448220b9158eab1612f4b6aae7d1514
Dynamo <chickabamboombam@gmail.com>
2025-05-22 01:41:04 -0400

    Updated StarterGui/BadgeDisplay/Settings/Notes.lua

---
+++ StarterGui/BadgeDisplay/Settings/Notes.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: Script
+-- Courtesy of Dynamo (@Roller_Bott)
+--[[
+
+// NOTES
+> Resizing the Container will not affect the scripts!
+> Tweentime is the length of time it takes for the window to open and close!
+> Downtime is the amount of time between each time the list of badges is updated ("refreshed")!
+> BadgeIDS is where you can add your own badges!
+> Colors is a configuration with attributes that represent the colors that show if the user does or does not have a badge!
+
+]]
