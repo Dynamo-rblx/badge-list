488be4c131469455054bf03b5167c2902a3e0d4d
Dynamo <chickabamboombam@gmail.com>
2025-05-22 01:41:06 -0400

    Updated StarterGui/BadgeDisplay/Settings/BadgeIDs.lua

---
+++ StarterGui/BadgeDisplay/Settings/BadgeIDs.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- replace the numbers with your own badge IDs
+-- you can insert as many as you need
+
+local badgeIds = {
+	-- example IDs:
+	3123078660681386,
+	580028713926830
+}
+
+return badgeIds
